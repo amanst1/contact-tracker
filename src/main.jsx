1f3709758d7a3e4396604c57cf60b56ebd882c35
amanst1 <seifeamanuel@gmail.com>
2023-01-25 23:56:40 +0300

    Make delete button work

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Root, { loader as rootLoader, action as rootAction } from "./routes/root"
 import ErrorPage from './error-page'
 import Contact, { loader as contactLoader } from './routes/contact'
 import EditContact, { action as editAction} from './routes/edit'
+import { action as destroyAction } from './routes/destroy'
 
 const router = createBrowserRouter([
   {
@@ -21,10 +22,8 @@ const router = createBrowserRouter([
         loader: contactLoader,
       },
       {
-        path: "contacts/:contactId/edit",
-        element: <EditContact />, 
-        loader: contactLoader,
-        action: editAction,
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
       },
     ],
   },
